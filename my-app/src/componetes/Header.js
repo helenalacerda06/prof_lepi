ae09bd3f5375e1a438f74169fa43ed2cbfbfea88
Helena Lacerda <helenacardoso062009@gmail.com>
2026-07-25 18:34:40 -0300

    alteracao no header

--- my-app/src/componetes/Header.js
+++ my-app/src/componetes/Header.js
@@ -1,8 +1,15 @@
 function Header() {
+  const headerStyle = {
+    backgroundImage: `linear-gradient(rgba(40, 44, 52, 0.75), rgba(40, 44, 52, 0.75)), url(${process.env.PUBLIC_URL}/professorLepi.jpeg)`,
+    backgroundSize: 'cover',
+    backgroundPosition: 'center',
+    backgroundRepeat: 'no-repeat',
+  };
+
   return (
-    <header className="App-header">
+    <header className="App-header" style={headerStyle}>
       <div className="header-content">
-        <div className="logo">Prof. Lepi</div>
+        <div className="logo" style={{ color: '#830000' }}>Prof. Lepi</div>
         <nav className="header-nav">
           <a href="#home">Home</a>
           <a href="#sobre">Sobre</a>
