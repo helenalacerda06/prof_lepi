1dec3a0faff4a1864ca4f2a127947343d683c718
Helena Lacerda <helenacardoso062009@gmail.com>
2026-07-26 21:05:35 -0300

    Quem somos

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,11 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
 import Header from './componetes/Header';
+import QuemSomos from './componetes/QuemSomos';
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <QuemSomos />
     </div>
   );
 }
---
+++ my-app/src/componetes/QuemSomos.js
@@ -0,0 +1,12 @@
+import styles from './QuemSomos.module.css';
+
+function QuemSomos() {
+  return (
+    <div className={styles.QuemSomos}>
+      <h1>Quem Somos</h1>
+      <h2>Esse site é dedicado ao trabalho de Literatura do segundo ano do ensino médio do colégio Acesso.</h2>
+      <p>Aqui vamos divulgar informações sobre os livros da federal, de uma forma divertida com a ajuda do professor Lepi, a traça.</p>
+    </div>
+  );
+}
+export default QuemSomos;
